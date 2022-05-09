056a1d0cce300f0c63b1b9a2a47c45980ef5cacc
SV Curious Cat <svcuriouscat@protonmail.com>
2022-05-08 22:22:27 -0400

    make website generator create CNAME

---
+++ website-generator.d/40-cname.py
@@ -0,0 +1,13 @@
+## Responsible for creating CNAME file
+## This file points GitHub pages website at custom domain
+
+import utils
+
+def stage(data):
+    fileHandle = utils.mkfile(
+        data["definitions"]["runtime"]["cwd"],
+        data["config"]["Filesystem"]["DestinationDirPath"],
+        "CNAME",
+    )
+    fileHandle.write("svcuriouscat.com")
+    fileHandle.close()
